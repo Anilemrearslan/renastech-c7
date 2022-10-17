2f15454153cc1c50846ff2eeb6086d6db97fdaf3
Anilemrearslan <anilemre167@gmail.com>
2022-10-16 21:28:16 -0400

    homework

--- src/homework/homework_13.java
+++ src/homework/homework_13.java
@@ -1,5 +1,7 @@
 package homework;
 
+import java.util.Scanner;
+
 public class homework_13 {
 //    Write a calculator program by following instructions:
 //   1) Ask the user to enter 1st number
@@ -12,4 +14,26 @@ public class homework_13 {
 //       Select operation  enter 1 for Summation, 2 for Subtraction, 3 for Multiplication, and 4 for division.: 1
 //                     The result is : 45.)
 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("please enter first number");
+        double number1= scan.nextDouble();
+        System.out.println("Please enter second number");
+        double number2= scan.nextDouble();
+        System.out.println("select operation enter 1 for Summation, 2 for Subtraction, 3 for Multiplication, and 4 for division.");
+        double operation =scan.nextDouble();
+
+        if (operation == 1){
+            System.out.println("Summation "+(number1+number2));
+        }else if (operation==2){
+            System.out.println("Subtraction "+(number1 - number2));
+        }else if (operation==3){
+            System.out.println("Multiplication "+(number1 * number2));
+        }else if (operation==4){
+            System.out.println("division "+(number1 / number2));
+        }else {
+            System.out.println("invalid number entered");
+        }
+    }
 }
