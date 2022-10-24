b00c1fde0dfbd7f62f45f994e48c5ad7b1e1d438
Anilemrearslan <anilemre167@gmail.com>
2022-10-24 19:17:00 -0400

    homework

--- src/homework/homework_27.java
+++ src/homework/homework_27.java
@@ -1,6 +1,32 @@
 package homework;
 
+import java.util.Scanner;
+
 public class homework_27 {
 //    Write a code which asks user to enter a String and removes all the duplicate characters from the String and prints the result
 //    ex. "AAAAAVVVCCDDAA" ==> "AVCD"
-}
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String input = scan.nextLine();
+        String output="";
+
+        for (int i = 0; i < input.length(); i++) {
+            if(!output.contains(String.valueOf(input.charAt(i)))) {
+                output += String.valueOf(input.charAt(i));
+            }
+                }
+        System.out.println(output);
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+
