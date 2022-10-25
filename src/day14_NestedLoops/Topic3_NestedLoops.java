0a06f7c6bebd6a9d28dad798f18c91ecdbe9b0c8
Anilemrearslan <anilemre167@gmail.com>
2022-10-24 21:15:20 -0400

    class

--- src/day14_NestedLoops/Topic3_NestedLoops.java
+++ src/day14_NestedLoops/Topic3_NestedLoops.java
@@ -7,10 +7,16 @@ public class Topic3_NestedLoops {
         // Assume you have 3 different fruit each have 10. you want to give 10 people equally each one of the fruit
 
         for (int i = 0; i < 3; i++) {
+
             System.out.println("The distribution: "+i);
             for (int j=1; j<=10; j++){
                 System.out.println("fruit: "+i+"for the kids: "+j);
             }
+
         }
+        System.out.println("The fruits are distributed: ");
+
+
+
     }
 }
