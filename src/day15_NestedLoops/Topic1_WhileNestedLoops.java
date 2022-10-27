7fe02da5c9c731720de33536db1cc055ce19fa9b
Anilemrearslan <anilemre167@gmail.com>
2022-10-27 19:20:40 -0400

    class

---
+++ src/day15_NestedLoops/Topic1_WhileNestedLoops.java
@@ -0,0 +1,32 @@
+package day15_NestedLoops;
+
+public class Topic1_WhileNestedLoops {
+
+    public static void main(String[] args) {
+      /*
+        while(condition){
+            //code block
+            while (condition2){
+                //code block
+            }
+        }
+        */
+
+        int i=0;
+        while(i < 10){
+
+            System.out.println("\t the value of i: "+i);
+
+            int j=1;
+            while(j<=10){
+                System.out.println("for each i: "+i+" the value of j: "+j);
+                j++;
+            }
+            i++;
+
+        }
+
+
+
+    }//end of main
+}//end of class
